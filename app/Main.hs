3dc6b1715d63f75f5f4526eacd505abe5dcb7f43
vimes <grass.sun.sky@gmail.com>
2021-01-24 23:11:32 +0000

    Add mass force calculation

--- app/Main.hs
+++ app/Main.hs
@@ -25,11 +25,26 @@ sumSquares _ _ = 0
 
 distance x y = sqrt (sumSquares x y)
 
+displacement (x:xs) (y:ys) = x-y : displacement xs ys
+displacement _ _ = []
+
+
+
 data Point = Point 
   {
       s :: [Double]
      ,v :: [Double]
   }
+  deriving (Read,Show)
+
+data Mass = Mass 
+  {
+      p :: Point
+    , m :: Double
+  }
+  deriving (Read,Show)
+
+data Force = Force [Double]
   deriving (Read,Show)
 
 pointDistance :: Point -> Point -> Double
@@ -41,12 +56,31 @@ p3 = Point [-1,-1] [0,0]
 
 p4 = Point [0,0] [1,0]
 
+m1 = Mass (Point [0.0,0.0] [1.0]) 1.0
+m2 = Mass (Point [1.0,1.0] [1.0]) 1.0
+m3 = Mass (Point [2.0,2.0] [1.0]) 1.0
+m4 = Mass (Point [3.0,3.0] [1.0]) 1.0
+m5 = Mass (Point [-1.0,-1.0] [1.0]) 1.0
+m6 = Mass (Point [1.0,0.0] [1.0]) 1.0
+
 update :: Point -> (Point,Point)
 update p = (p',p')
   where 
     p' = Point x y
       where x = addList (s p) (v p)
             y = v p
+
+massForce :: Double -> Mass -> Mass -> Force
+massForce g x y = Force (map (*magnitude) (displacement (s (p x)) (s (p y))))
+                    where 
+                      magnitude = (g*(m x)*(m y))/(s^2)
+                        where s = pointDistance (p x) (p y)
+
+
+-- massAccelerate :: [Mass] -> ([Mass],[Mass])
+-- massAccelerate masses = (masses', masses)
+--   where
+--     masses' = 
 
 moveSeries :: State Point [Point]
 moveSeries = do
@@ -65,6 +99,10 @@ moveWrite :: StateT Point IO ()
 moveWrite = forever $ do
   a1 <- state (update)
   liftIO $ print a1
+
+-- movePair :: StateT [Mass] IO ()
+-- movePair = forever $ do
+
 
 -- main :: IO ()
 main = runStateT moveWrite p4 -- start loop at 10
