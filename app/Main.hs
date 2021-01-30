848c127582b2f9b9a7bf1bf5f2dbaa579ba7d39f
vimes <grass.sun.sky@gmail.com>
2021-01-30 13:15:28 +0000

    It works now

--- app/Main.hs
+++ app/Main.hs
@@ -12,8 +12,9 @@ import Control.Monad.Trans.State
 import Control.Monad (forM_, when)
 import Control.Monad.Writer (WriterT, execWriterT, runWriterT, tell)
 
-
--- import Control.Monad.Trans (liftIO)
+delete i items = take (i-1) items ++ drop i items
+eliminate items i = (delete i items,items !! (i-1))
+recombine (x,xs) = x:xs
 
 addList :: [Double] -> [Double] -> [Double]
 addList (x:xs) (y:ys) = x+y : addList xs ys
@@ -41,6 +42,7 @@ data Mass = Mass
   {
       p :: Point
     , m :: Double
+    , inertia :: Double
   }
   deriving (Read,Show)
 
@@ -53,58 +55,86 @@ p3 = Point [-1,-1] [0,0]
 
 p4 = Point [0,0] [1,0]
 
-m1 = Mass (Point [0.0,0.0] [1.0]) 1.0
-m2 = Mass (Point [1.0,1.0] [1.0]) 1.0
-m3 = Mass (Point [2.0,2.0] [1.0]) 1.0
-m4 = Mass (Point [3.0,3.0] [1.0]) 1.0
-m5 = Mass (Point [-1.0,-1.0] [1.0]) 1.0
-m6 = Mass (Point [1.0,0.0] [1.0]) 1.0
+m1 = Mass (Point [0.0,0.0,0.0] [0.0,0.0,0.0]) 1.0 1.0
+m2 = Mass (Point [1.0,1.0,1.0] [0.0,0.0,0.0]) 1.0 1.0
+m3 = Mass (Point [2.0,2.0,2.0] [0.0,0.0,0.0]) 1.0 1.0
+m4 = Mass (Point [3.0,3.0,3.0] [-4.0,-2.0,1.0]) 1.0 1.0
+m5 = Mass (Point [-1.0,-1.0,-2.0] [1.0,3.0,4.0]) 1.0 1.0
+m6 = Mass (Point [1.0,0.0,-3.0] [1.0,-1.0,-1.5]) 1.0 1.0
 
-update :: Point -> (Point,Point)
-update p = (p',p')
+updatePoint :: Point -> (Point,Point)
+updatePoint p = (p',p')
   where 
     p' = Point x y
       where x = addList (s p) (v p)
             y = v p
 
-massForce :: Double -> Mass -> Mass -> [Double]
-massForce g x y = map (*magnitude) (displacement (s (p x)) (s (p y)))
+updateMasses :: [Mass] -> ([Mass],[Mass])
+updateMasses ms = (ms',ms')
   where 
-    magnitude = (g*(m x)*(m y))/(s^2)
+    ms' = map f ms
+      where f mass = Mass (Point (addList (s (p mass)) (v (p mass))) (v (p mass))) (m mass) (inertia mass)
+
+acccelerateMassesOneState :: [Mass] -> ([Mass],[Mass])
+acccelerateMassesOneState ms = (ms',ms')
+  where ms' = accelerateMassesOne ms
+
+massAcceleration :: Double -> Mass -> Mass -> [Double]
+massAcceleration g x y = map (*magnitude) (displacement (s (p x)) (s (p y)))
+  where 
+    magnitude = (g*(m x)*(m y))/((s^2)*inertia x)
       where s = pointDistance (p x) (p y)
 
-totalMassForce :: Double -> Mass -> [Mass] -> [Double]
-totalMassForce g mass masses = foldl1 addList (map (massForce g mass) masses)
+totalMassAcceleration :: Double -> Mass -> [Mass] -> [Double]
+totalMassAcceleration g mass masses = foldl1 addList (map (massAcceleration g mass) masses)
 
--- accelerateMasses :: [Mass] -> [Mass]
--- accelerateMasses 
+accelerateMass :: [Double] -> Mass -> Mass
+accelerateMass acceleration mass = mass'
+  where mass' = Mass (Point (s (p mass)) (addList (v (p mass)) acceleration)) (m mass) (inertia mass)
 
--- massAccelerate :: [Mass] -> ([Mass],[Mass])
--- massAccelerate masses = (masses', masses)
---   where
---     masses' = 
+accelerateMassFromGravForce g masses mass = mass'
+  where mass' = accelerateMass (totalMassAcceleration g mass masses) mass 
+
+accelerateMassFromGravForceTuple g (masses,mass) = accelerateMassFromGravForce g masses mass
+
+generateInteractions masses = interactions
+  where interactions = map (eliminate masses) [1..length masses] 
+
+accelerateMasses :: Double -> [Mass] -> [Mass]
+accelerateMasses g masses = masses'
+  where masses' = map (accelerateMassFromGravForceTuple g) (generateInteractions masses)
+
+accelerateMassesOne :: [Mass] -> [Mass]
+accelerateMassesOne = accelerateMasses 20.01
 
 moveSeries :: State Point [Point]
 moveSeries = do
-  a1 <- state (update)
-  a2 <- state (update)
-  a3 <- state (update)
+  a1 <- state (updatePoint)
+  a2 <- state (updatePoint)
+  a3 <- state (updatePoint)
   return [a1,a2,a3]
 
 stuckWrite :: StateT Point (WriterT String IO) ()
 stuckWrite = do
-  a1 <- state (update)
+  a1 <- state (updatePoint)
   _ <- (lift . tell) (printf "\n" ++ show a1)
   return ()
 
 moveWrite :: StateT Point IO ()
 moveWrite = forever $ do
-  a1 <- state (update)
+  a1 <- state (updatePoint)
   liftIO $ print a1
+
+simulateSystem :: StateT [Mass] IO ()
+simulateSystem = forever $ do
+  a1 <- state (updateMasses)
+  a2 <- state (acccelerateMassesOneState)
+  liftIO $ print a1
+  liftIO $ print a2
 
 -- movePair :: StateT [Mass] IO ()
 -- movePair = forever $ do
 
 
 -- main :: IO ()
-main = runStateT moveWrite p4 -- start loop at 10
+main = runStateT simulateSystem [m1,m2,m3] -- start loop at 10
